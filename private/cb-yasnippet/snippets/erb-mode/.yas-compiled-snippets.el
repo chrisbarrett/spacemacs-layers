ab7b9e78e94e18637948c5410a69dc20ef1c4eb0
Chris Barrett <chris.d.barrett@me.com>
2014-12-01 10:26:18 +1300

    Removecompiled snippets, update gitignore

--- private/cb-yasnippet/snippets/erb-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `erb-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'erb-mode
-                     '(("%" "<% $0 %>" "erb interpolation" nil nil nil nil nil nil)
-                       ("%=" "<%= $0 %>" "erb interpolation 2" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Sat Nov 29 10:54:35 2014
--- private/cb-yasnippet/snippets/scala-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `scala-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'scala-mode
-                     '(("ccls" "case class $1 ${2:(${3:arg} : ${4:type}) }extends ${5:`(cbscala:find-case-class-parent)`}\n$0" "case class" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Sat Nov 29 10:54:35 2014
