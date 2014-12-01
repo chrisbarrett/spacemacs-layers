fde4a9f22abea80e1df9acaedbeef8000a9e1e90
Chris Barrett <chris.d.barrett@me.com>
2014-12-02 00:12:37 +1300

    Remove redundant funcs

--- private/cb-cosmetic/funcs.el
+++ private/cb-cosmetic/funcs.el
@@ -1,19 +1,3 @@
-;;; Color themes
-
-(defun solarized-light ()
-  "Switch theme to solarized light."
-  (interactive)
-  (load-theme 'solarized-light 'no-confirm))
-
-(defun solarized-dark ()
-  "Switch theme to solarized dark."
-  (interactive)
-  (load-theme 'solarized-dark 'no-confirm))
-
-(defalias 'light 'solarized-light)
-(defalias 'dark 'solarized-dark)
-
-
 ;;; Whitespace
 
 (defun core/set-whitespace-mode ()
