215b82d20bf39c98f34a76045fc119170dc9c0b6
Chris Barrett <chris.d.barrett@me.com>
2014-12-02 22:14:17 +1300

    Set scalastyle flycheck vars

---
+++ private/cb-scala/config.el
@@ -0,0 +1,3 @@
+(setq flycheck-scalastyle-jar "~/bin/scalastyle/lib/scalastyle_2.10-0.5.0.jar")
+(setq flycheck-scalastylerc "~/bin/scalastyle/scalastyle_config.xml")
+(setq flycheck-scala-scalastyle-executable "~/bin/scalastyle/scalastyle-batch_2.10.jar")
