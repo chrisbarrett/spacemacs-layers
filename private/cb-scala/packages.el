1190c12cdc2aba67bcc8ea0b725ead8af9f1e235
Chris Barrett <chris.d.barrett@me.com>
2014-12-02 22:19:36 +1300

    Configure ensime

--- private/cb-scala/packages.el
+++ private/cb-scala/packages.el
@@ -2,6 +2,7 @@
   '(
     ;; package cb-scalas go here
     scala-mode2
+    ensime
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -26,3 +27,9 @@ which require an initialization must be listed explicitly in the list.")
       (setq scala-indent:align-forms t)
       (setq scala-indent:align-parameters t)
       (setq scala-indent:default-run-on-strategy scala-indent:eager-strategy))))
+
+(defun cb-scala/init-ensime ()
+  (use-package ensime
+    :defer t
+    :init
+    (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
