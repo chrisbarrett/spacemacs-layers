d9ddc0f12c3f6343745564fa167eeeadb09eb879
Chris Barrett <chris.d.barrett@me.com>
2014-12-29 19:27:16 +1300

    Scala keybindings

--- private/cb-scala/keybindings.el
+++ private/cb-scala/keybindings.el
@@ -1,33 +1,43 @@
 (after 'scala-mode2
   (define-key scala-mode-map (kbd ".") nil)
-  (evil-define-key 'normal scala-mode-map "J" 'scala/join-line)
   (define-key scala-mode-map (kbd "M-RET") 'scala/meta-ret)
 
-  (define-key scala-mode-map (kbd "C-c C-z") 'ensime-inf-switch)
-  (define-key scala-mode-map (kbd "C-c C-l") 'ensime-inf-load-file)
-  (define-key scala-mode-map (kbd "M-q") 'ensime-format-source)
+  (evil-define-key 'normal scala-mode-map "J" 'scala/join-line)
 
-  (define-key scala-mode-map (kbd "C-c C-r C-r") 'ensime-refactor-rename)
-
-  (define-key scala-mode-map (kbd "C-c C-h") 'ensime-show-doc-for-symbol-at-point)
-  (evil-leader/set-key-for-mode 'scala-mode "ii" 'ensime-insert-import)
-
-  (evil-define-key 'insert scala-mode-map (kbd "SPC") 'scala/smart-space)
-  (evil-define-key 'insert scala-mode-map (kbd "<backspace>") 'scala/backspace)
-  (evil-define-key 'insert scala-mode-map (kbd "<return>") 'scala/ret)
+  (evil-define-key 'insert scala-mode-map
+    (kbd "SPC") 'scala/smart-space
+    (kbd "<backspace>") 'scala/backspace
+    (kbd "<return>") 'scala/ret)
   )
 
 (after 'ensime
   (define-key ensime-inf-mode-map (kbd "C-c C-z") 'scala/switch-to-src)
+  (define-key ensime-mode-map (kbd "C-c C-z") 'ensime-inf-switch)
+  (define-key ensime-mode-map (kbd "C-c C-l") 'ensime-inf-load-file)
+  (define-key ensime-mode-map (kbd "M-q") 'ensime-format-source)
+  (define-key ensime-mode-map (kbd "C-c C-r C-r") 'ensime-refactor-rename)
+  (define-key ensime-mode-map (kbd "C-c C-h") 'ensime-show-doc-for-symbol-at-point)
   (define-key ensime-mode-map (kbd "M-N") 'ensime-forward-note)
   (define-key ensime-mode-map (kbd "M-P") 'ensime-backward-note)
 
-  (evil-define-key 'insert ensime-inf-mode-map (kbd "SPC") 'scala/smart-space)
-  (evil-define-key 'insert ensime-inf-mode-map (kbd "<backspace>") 'scala/backspace)
+  (evil-leader/set-key-for-mode 'scala-mode "ii" 'ensime-insert-import)
+
+  (evil-define-key 'insert ensime-inf-mode-map
+    (kbd "SPC") 'scala/smart-space
+    (kbd "<backspace>") 'scala/backspace)
+
+  (evil-define-key 'normal ensime-popup-buffer-map
+    (kbd "q") 'ensime-popup-buffer-quit-function)
 
   (evil-define-key 'normal ensime-refactor-info-map
+    (kbd "q") 'scala/ensime-refactor-cancel
     (kbd "c") 'scala/ensime-refactor-accept
     (kbd "RET") 'scala/ensime-refactor-accept)
 
-  (evil-define-key 'normal ensime-refactor-info-map (kbd "q")
-    'scala/ensime-refactor-cancel))
+  (evil-define-key 'normal ensime-compile-result-map
+    (kbd "g") 'ensime-show-all-errors-and-warnings
+    (kbd "TAB") 'forward-button
+    (kbd "<backtab>") 'backward-button
+    (kbd "M-n") 'forward-button
+    (kbd "M-p") 'backward-button)
+  )
