e3f73e9e5095ee98890f321efbb1bab2cb144208
Chris Barrett <chris.d.barrett@me.com>
2015-01-08 13:55:52 +1300

    use relative line numbers

--- private/cb-core/packages.el
+++ private/cb-core/packages.el
@@ -16,6 +16,7 @@
     flycheck
     use-package
     aggressive-indent
+    relative-line-numbers
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
