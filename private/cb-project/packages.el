1eaa11f61a60495ab0912641507a076d70ed89cb
Chris Barrett <chris.d.barrett@me.com>
2015-01-19 19:23:45 +1300

    Require grizzl

--- private/cb-project/packages.el
+++ private/cb-project/packages.el
@@ -3,6 +3,7 @@
     ;; package cb-projects go here
     projectile
     skeletor
+    grizzl
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
