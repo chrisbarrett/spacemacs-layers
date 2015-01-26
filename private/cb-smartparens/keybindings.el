d46a63e5f43f2f29e331ce73811fe02e6b8200e7
Chris Barrett <chris.d.barrett@me.com>
2015-01-27 08:40:19 +1300

    Tweak sp bindings

--- private/cb-smartparens/keybindings.el
+++ private/cb-smartparens/keybindings.el
@@ -8,33 +8,36 @@
   (define-key sp-keymap (kbd "C-k") 'sp/kill-blank-lines)
   (define-key sp-keymap (kbd "DEL") 'sp-backward-delete-char))
 
-(evil-global-set-key 'normal ",,A" 'sp-add-to-previous-sexp)
-(evil-global-set-key 'normal ",,a" 'sp-add-to-next-sexp)
-(evil-global-set-key 'normal ",,B" 'sp-backward-barf-sexp)
-(evil-global-set-key 'normal ",,b" 'sp-forward-barf-sexp)
-(evil-global-set-key 'normal ",,M" 'sp-backward-slurp-sexp)
-(evil-global-set-key 'normal ",,m" 'sp-forward-slurp-sexp)
-(evil-global-set-key 'normal ",,c" 'sp-convolute-sexp)
-(evil-global-set-key 'normal ",,D" 'sp-backward-kill-sexp)
-(evil-global-set-key 'normal ",,d" 'sp-kill-sexp)
-(evil-global-set-key 'normal ",,e" 'sp-emit-sexp)
-(evil-global-set-key 'normal ",,l" 'sp-end-of-sexp)
-(evil-global-set-key 'normal ",,h" 'sp-beginning-of-sexp)
-(evil-global-set-key 'normal ",,j" 'sp-join-sexp)
-(evil-global-set-key 'normal ",,K" 'sp-splice-sexp-killing-backward)
-(evil-global-set-key 'normal ",,k" 'sp-splice-sexp-killing-forward)
-(evil-global-set-key 'normal ",,n" 'sp-next-sexp)
-(evil-global-set-key 'normal ",,p" 'sp-previous-sexp)
-(evil-global-set-key 'normal ",,r" 'sp-raise-sexp)
-(evil-global-set-key 'normal ",,s" 'sp-splice-sexp-killing-around)
-(evil-global-set-key 'normal ",,t" 'sp-transpose-sexp)
-(evil-global-set-key 'normal ",,U" 'sp-backward-unwrap-sexp)
-(evil-global-set-key 'normal ",,u" 'sp-unwrap-sexp)
-(evil-global-set-key 'normal ",,w" 'sp-rewrap-sexp)
-(evil-global-set-key 'normal ",,x" 'sp-split-sexp)
-(evil-global-set-key 'normal ",,Y" 'sp-backward-copy-sexp)
-(evil-global-set-key 'normal ",,y" 'sp-copy-sexp)
-(evil-global-set-key 'normal ",,," 'sp-previous-sexp)
-(evil-global-set-key 'normal ",,." 'sp-next-sexp)
-(evil-global-set-key 'normal ",,<" 'sp-backward-down-sexp)
-(evil-global-set-key 'normal ",,>" 'sp-down-sexp)
+(spacemacs/declare-prefix "," "smartparens")
+(evil-leader/set-key
+  ",A" 'sp-add-to-previous-sexp
+  ",a" 'sp-add-to-next-sexp
+  ",B" 'sp-backward-barf-sexp
+  ",b" 'sp-forward-barf-sexp
+  ",M" 'sp-backward-slurp-sexp
+  ",m" 'sp-forward-slurp-sexp
+  ",c" 'sp-convolute-sexp
+  ",D" 'sp-backward-kill-sexp
+  ",d" 'sp-kill-sexp
+  ",e" 'sp-emit-sexp
+  ",l" 'sp-end-of-sexp
+  ",h" 'sp-beginning-of-sexp
+  ",j" 'sp-join-sexp
+  ",K" 'sp-splice-sexp-killing-backward
+  ",k" 'sp-splice-sexp-killing-forward
+  ",n" 'sp-next-sexp
+  ",p" 'sp-previous-sexp
+  ",r" 'sp-raise-sexp
+  ",s" 'sp-splice-sexp-killing-around
+  ",t" 'sp-transpose-sexp
+  ",U" 'sp-backward-unwrap-sexp
+  ",u" 'sp-unwrap-sexp
+  ",w" 'sp-rewrap-sexp
+  ",x" 'sp-split-sexp
+  ",Y" 'sp-backward-copy-sexp
+  ",y" 'sp-copy-sexp
+  ",," 'sp-previous-sexp
+  ",." 'sp-next-sexp
+  ",<" 'sp-backward-down-sexp
+  ",>" 'sp-down-sexp
+  )
