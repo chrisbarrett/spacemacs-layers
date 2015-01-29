13a1ce35e708a7d4058b65b003c10c8c19dbb80a
Chris Barrett <chris.d.barrett@me.com>
2015-01-30 11:27:10 +1300

    Update font size setting for spacemacs changes

--- private/cb-core/config.el
+++ private/cb-core/config.el
@@ -7,12 +7,7 @@
 
 ;;; Use larger font
 (defvar core/monospace-font "Source Code Pro")
-(spacemacs/set-font core/monospace-font 12)
-
-(defun set-font-size (size)
-  "Set the font size to use for all frames"
-  (interactive (list (read (ido-completing-read "Size: " (list "10" "12" "14" "18")))))
-  (spacemacs/set-font core/monospace-font size))
+(spacemacs/set-default-font (list core/monospace-font :size 12))
 
 (defvar spacemacs-private-directory (concat user-emacs-directory "private/"))
 (defvar spacemacs-autosaves-directory (concat user-emacs-directory "autosaves/"))
