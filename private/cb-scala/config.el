1bc3e50a80160a3b5565c3f01a000d34b714a2d3
Chris Barrett <chris.d.barrett@me.com>
2015-02-03 08:36:59 +1300

    bump scala version

--- private/cb-scala/config.el
+++ private/cb-scala/config.el
@@ -1,5 +1,6 @@
 (setq flycheck-scalastyle-jar "~/bin/scalastyle/lib/scalastyle_2.10-0.5.0.jar")
 (setq flycheck-scalastylerc "~/bin/scalastyle/scalastyle_config.xml")
 (setq flycheck-scala-scalastyle-executable "~/bin/scalastyle/scalastyle-batch_2.10.jar")
+(setq ensime-default-scala-version "2.11.5")
 
 (add-to-list 'core/indent-commands-alist '(scala-mode . ensime-format-source))
