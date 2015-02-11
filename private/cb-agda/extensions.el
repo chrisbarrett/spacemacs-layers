ba154ac466fd613596c34c0e4f7bddc8a1d44d1d
Chris Barrett <chris.d.barrett@me.com>
2015-02-11 21:32:10 +1300

    Add agda-mode code as an extension

--- private/cb-agda/extensions.el
+++ private/cb-agda/extensions.el
@@ -11,30 +11,56 @@
 ;;; License: GPLv3
 
 (defvar cb-agda-pre-extensions
-  '(
-    ;; pre extension cb-agdas go here
-    super-smart-ops
-    )
+  '(agda-mode)
   "List of all extensions to load before the packages.")
 
 (defvar cb-agda-post-extensions
-  '(
-    ;; post extension cb-agdas go here
-    )
+  '(super-smart-ops
+    aggressive-indent)
   "List of all extensions to load after the packages.")
 
-;; For each extension, define a function cb-agda/init-<extension-cb-agda>
-;;
-;; (defun cb-agda/init-my-extension ()
-;;   "Initialize my extension"
-;;   )
-;;
-;; Often the body of an initialize function uses `use-package'
-;; For more info on `use-package', see readme:
-;; https://github.com/jwiegley/use-package
 
 (defun cb-agda/init-super-smart-ops ()
   (use-package super-smart-ops
     :config
     (super-smart-ops-configure-for-mode 'agda2-mode
       :add '("$"))))
+
+(defun cb-agda/init-aggressive-indent ()
+  (use-package aggressive-indent
+    :config
+    (add-to-list 'aggressive-indent-excluded-modes 'agda2-mode)))
+
+(defun cb-agda/init-agda-mode ()
+  (use-package agda2
+    :init
+    (progn
+      (defun cb-agda/configure-agda-mode-hooks ()
+        (add-hook 'after-save-hook 'agda2-load nil t)
+        (add-hook 'before-save-hook 'agda/rewrite-symbols-in-buffer nil t))
+
+      (add-hook 'agda2-mode-hook 'cb-agda/configure-agda-mode-hooks))
+    :config
+    (progn
+      (add-to-list 'face-remapping-alist '(agda2-highlight-error-face . flycheck-error))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-keyword-face . font-lock-keyword-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-bound-variable-face . font-lock-variable-name-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-module-face . font-lock-constant-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-datatype-face . font-lock-type-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-record-face . font-lock-type-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-function-face . default))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-primitive-type-face . font-lock-builtin-face))
+      (add-to-list 'face-remapping-alist '(agda2-highlight-symbol-face . default))
+
+      (custom-set-faces
+       `(agda2-highlight-number-face
+         ((t
+           (:foreground ,solarized-hl-magenta))))
+
+       `(agda2-highlight-inductive-constructor-face
+         ((t
+           (:foreground ,solarized-hl-violet :italic t))))
+
+       `(agda2-highlight-coinductive-constructor-face
+         ((t
+           (:foreground ,solarized-hl-magenta :italic t))))))))
--- private/cb-agda/keybindings.el
+++ private/cb-agda/keybindings.el
@@ -1,16 +1,22 @@
 (after 'agda2-mode
   (evil-define-key 'normal agda2-mode-map
     ",l" 'agda2-load
-    ",c" 'agda2-compute-normalised
-    ",C" 'agda2-compile
-    ",d" 'agda2-infer-type
-    ",g" 'agda2-show-goals
+    ",c" 'agda2-make-case
+    ",n" 'agda2-compute-normalised
+    ",g" 'agda2-give
+    ",r" 'agda2-refine
+
+    ",a" 'agda2-solveAll
+    ",sc" 'agda2-show-constraints
+    ",sg" 'agda2-show-goals
+    ",t" 'agda2-infer-type
+
     ",h" 'agda2-display-implicit-arguments
-    ",n" 'agda2-next-goal
-    ",N" 'agda2-previous-goal
-    ",s" 'agda2-show-constraints
-    ",S" 'agda2-solveAll
-    ",x" 'agda2-restart)
+    ",R" 'agda2-restart
+
+    (kbd "M-.") 'agda2-goto-definition-keyboard
+    (kbd "M-,") 'agda2-go-back
+    )
 
   (define-key agda2-mode-map (kbd "M-RET") 'agda/meta-ret)
   (define-key agda2-mode-map (kbd "M-N") 'agda2-next-goal)
