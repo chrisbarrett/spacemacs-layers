eef715ed9c6f619b7b227596968df6c17e02b257
Chris Barrett <chris.d.barrett@me.com>
2015-03-03 22:12:35 +1300

    Disable ghc-mod

--- private/cb-haskell/packages.el
+++ private/cb-haskell/packages.el
@@ -7,7 +7,7 @@
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defvar cb-haskell-excluded-packages '()
+(defvar cb-haskell-excluded-packages '(ghc)
   "List of packages to exclude.")
 
 (defun cb-haskell/init-haskell-mode ()
