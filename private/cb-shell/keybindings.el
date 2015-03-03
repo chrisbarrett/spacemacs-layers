a9d89fc243b77522d4b8c3563b57248feceb6b2a
Chris Barrett <chris.d.barrett@me.com>
2015-03-03 19:31:35 +1300

    Switch term input mode with C-c RET

--- private/cb-shell/keybindings.el
+++ private/cb-shell/keybindings.el
@@ -1,3 +1,9 @@
 (global-set-key (kbd "<f1>") 'cb-shell-term-bring)
 
 (evil-set-initial-state 'term-mode 'emacs)
+
+(defadvice term-char-mode (after set-keybindings activate)
+  (local-set-key (kbd "C-c RET") 'term-line-mode))
+
+(defadvice term-line-mode (after set-keybindings activate)
+  (local-set-key (kbd "C-c RET") 'term-char-mode))
