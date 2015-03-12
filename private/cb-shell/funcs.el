33bbb71b3d0ddffe43e725112b6ff9e426db6424
Chris Barrett <chris.d.barrett@me.com>
2015-03-12 13:04:38 +1300

    shell: shell-bring uses different shells for different frames

--- private/cb-shell/funcs.el
+++ private/cb-shell/funcs.el
@@ -16,25 +16,36 @@ With prefix argument ARG, always create a new terminal."
     (cb-shell--term-new))))
 
 (defun cb-shell--term-hide ()
-  (if (get-register 'term)
-      (or (ignore-errors (jump-to-register 'term t) t)
-          (bury-buffer))
-    (bury-buffer)
-    (when (< 1 (length (window-list)))
-      (delete-window))))
+  (let ((reg (cb-shell--mk-register-name)))
+    (if (get-register reg)
+        (or (ignore-errors (jump-to-register reg t) t)
+            (bury-buffer))
+      (bury-buffer)
+      (when (< 1 (length (window-list)))
+        (delete-window)))))
 
 (defun cb-shell--term-new ()
-  (window-configuration-to-register 'term)
+  (window-configuration-to-register (cb-shell--mk-register-name))
   (save-window-excursion
     (let ((shell (or explicit-shell-file-name (getenv "SHELL"))))
       (ansi-term shell)))
   (cb-shell--term-show))
 
 (defun cb-shell--term-show ()
-  (window-configuration-to-register 'term)
+  (window-configuration-to-register (cb-shell--mk-register-name))
   (pop-to-buffer (cb-shell--term-buffer)))
 
 (defun cb-shell--term-buffer ()
-  (--first (with-current-buffer it
-             (derived-mode-p 'term-mode))
-           (buffer-list)))
+  (let ((current-frame (cb-shell--current-frame)))
+    (--first (with-current-buffer it
+               (and (derived-mode-p 'term-mode)
+                    (s-matches? "ansi-term" (buffer-name it))
+                    (equal current-frame (window-frame (get-buffer-window it)))))
+             (buffer-list))))
+
+(defun cb-shell--current-frame ()
+  (window-frame (get-buffer-window (current-buffer))))
+
+(defun cb-shell--mk-register-name ()
+  (-let [(&alist 'window-id id) (frame-parameters (cb-shell--current-frame))]
+    (intern (format "cb-term-%s" id))))
