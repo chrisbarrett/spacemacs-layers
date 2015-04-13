d0609ef59724491c6ea74f475030c3c0ec57f555
Chris Barrett <chris.d.barrett@me.com>
2015-04-13 21:56:44 +1200

    haskell: remove more filepaths from flycheck error messages

--- private/cb-flycheck/config.el
+++ private/cb-flycheck/config.el
@@ -1,16 +1,16 @@
 ;;; Hide tmp file paths from error output
 
 (defconst cb-flycheck--src-file-rx
-  (rx bol (+ space) (? "(bound ") "at /var/" (* nonl) eol))
+  (rx bol (+ space) (? "(bound ") "at /" (* nonl) eol))
 
-(defun cb-flycheck--strip-tmp-filepath (str)
+(defun cb-flycheck--strip-filepath (str)
   (->> (s-split "\n" str)
        (--remove (s-matches? cb-flycheck--src-file-rx it))
        (s-join "\n")))
 
-(defun cb-flycheck-strip-tmpfiles-in-messages (errors)
+(defun cb-flycheck-strip-files-in-messages (errors)
   (dolist (err errors)
-    (let ((message (cb-flycheck--strip-tmp-filepath (flycheck-error-message err))))
+    (let ((message (cb-flycheck--strip-filepath (flycheck-error-message err))))
       (setf (flycheck-error-message err) message)))
   errors)
 
@@ -18,4 +18,16 @@
   (put 'haskell-ghc
        'flycheck-error-filter
        (lambda (errors)
-         (flycheck-sanitize-errors (cb-flycheck-strip-tmpfiles-in-messages (flycheck-dedent-error-messages errors))))))
+         (-> errors
+             flycheck-dedent-error-messages
+             cb-flycheck-strip-files-in-messages
+             flycheck-sanitize-errors))))
+
+(with-eval-after-load 'flycheck-liquid
+  (put 'haskell-liquid
+       'flycheck-error-filter
+       (lambda (errors)
+         (-> errors
+             flycheck-dedent-error-messages
+             cb-flycheck-strip-files-in-messages
+             flycheck-sanitize-errors))))
