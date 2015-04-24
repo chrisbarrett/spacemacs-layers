a2001a6ece05c579417f15d46533e0b9300bdb49
Chris Barrett <chris.d.barrett@me.com>
2015-04-24 19:16:04 +1200

    yas: update keybindings

--- private/cb-yasnippet/keybindings.el
+++ private/cb-yasnippet/keybindings.el
@@ -1,10 +1,12 @@
 (require 'yasnippet)
 
 (bind-key "<backspace>" 'yas/backspace yas-keymap)
-(bind-key "SPC" 'yas/space yas-keymap)
+
+(with-eval-after-load 'yasnippet
+  (evil-define-key 'insert yas-keymap (kbd "SPC") 'yas/space))
 
 (spacemacs/declare-prefix "y" "yasnippet")
-(evil-leader/set-key "yf" 'yas/visit-snippet-file)
-(evil-leader/set-key "yn" 'yas/new-snippet)
-(evil-leader/set-key "yy" 'yas/insert-snippet)
+(evil-leader/set-key "yf" 'yas-visit-snippet-file)
+(evil-leader/set-key "yn" 'yas-new-snippet)
+(evil-leader/set-key "yy" 'yas-insert-snippet)
 (evil-leader/set-key "yr" 'yas//reload-all)
