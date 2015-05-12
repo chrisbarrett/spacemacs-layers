052d873946fa50a469dbcbc6358cac4c9d35c76b
Chris Barrett <chris.d.barrett@me.com>
2015-05-12 14:01:01 +1200

    flycheck: configure haskell-process checker

--- private/cb-flycheck/config.el
+++ private/cb-flycheck/config.el
@@ -1,4 +1,12 @@
-;;; Hide tmp file paths from error output
+;;; config.el --- Configuration for flycheck
+;;; Commentary:
+;;; Code:
+
+(require 's nil t)
+(require 'dash nil t)
+(require 'flycheck nil t)
+
+;;; Hide noisy Haskell file paths from error output
 
 (defconst cb-flycheck--src-file-rx
   (rx bol (+ space) (? "(bound ") "at /" (* nonl) eol))
@@ -10,8 +18,8 @@
 
 (defun cb-flycheck-strip-files-in-messages (errors)
   (dolist (err errors)
-    (let ((message (cb-flycheck--strip-filepath (flycheck-error-message err))))
-      (setf (flycheck-error-message err) message)))
+    (let ((msg (cb-flycheck--strip-filepath (flycheck-error-message err))))
+      (setf (flycheck-error-message err) msg)))
   errors)
 
 (with-eval-after-load 'flycheck
@@ -25,6 +33,15 @@
 
 (with-eval-after-load 'flycheck-liquid
   (put 'haskell-liquid
+       'flycheck-error-filter
+       (lambda (errors)
+         (-> errors
+             flycheck-dedent-error-messages
+             cb-flycheck-strip-files-in-messages
+             flycheck-sanitize-errors))))
+
+(with-eval-after-load 'haskell-flycheck
+  (put 'haskell-process
        'flycheck-error-filter
        (lambda (errors)
          (-> errors
---
+++ private/cb-flycheck/extensions.el
@@ -0,0 +1,27 @@
+;;; extensions.el --- cb-flycheck Layer extensions File for Spacemacs
+;;; Commentary:
+;;; Code:
+
+(defvar cb-flycheck-pre-extensions
+  '()
+  "List of all extensions to load before the packages.")
+
+(defvar cb-flycheck-post-extensions
+  '(haskell-flycheck)
+  "List of all extensions to load after the packages.")
+
+(eval-when-compile
+  (require 'use-package nil t))
+
+(defun cb-flycheck/init-haskell-flycheck ()
+  (use-package haskell-flycheck
+    :config
+    (progn
+      ;;; Use the Haskell session for syntax checking when available
+
+      (defun cb-flycheck/configure-haskell-checker ()
+        (flycheck-select-checker 'haskell-process)
+        (flycheck-mode +1))
+
+      (add-hook 'interactive-haskell-mode-hook 'cb-flycheck/configure-haskell-checker)
+      )))
