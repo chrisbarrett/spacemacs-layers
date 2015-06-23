441d068b3d93afdfb00b616a7d6a63c483037479
Chris Barrett <chris.d.barrett@me.com>
2015-06-23 14:26:30 +1200

    scala: set sbt buffer height

--- private/cb-scala/config.el
+++ private/cb-scala/config.el
@@ -3,3 +3,6 @@
 (setq flycheck-scala-scalastyle-executable "~/bin/scalastyle/scalastyle-batch_2.10.jar")
 
 (add-to-list 'core/indent-commands-alist '(scala-mode . ensime-format-source))
+
+(with-eval-after-load 'popwin
+  (push '("^\\*sbt\\*" :regexp t :dedicated t :position bottom :stick t :noselect nil :height 33) popwin:special-display-config))
