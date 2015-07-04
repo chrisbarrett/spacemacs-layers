926c1b1636808434d3dfc35b7875f5d5f4af62f2
Chris Barrett <chris.d.barrett@me.com>
2015-07-05 08:48:49 +1200

    sp: enable sp global strict mode

--- cb-smartparens/packages.el
+++ cb-smartparens/packages.el
@@ -2,12 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar cb-smartparens-packages
+(defconst cb-smartparens-packages
   '(smartparens)
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defvar cb-smartparens-excluded-packages '()
+(defconst cb-smartparens-excluded-packages '()
   "List of packages to exclude.")
 
 (eval-when-compile
@@ -15,23 +15,16 @@ which require an initialization must be listed explicitly in the list.")
 
 (defun cb-smartparens/init-smartparens ()
   (use-package smartparens
-    :commands (smartparens-global-mode show-smartparens-global-mode)
-    :init nil
-    (progn
-      (smartparens-global-mode)
-      (show-smartparens-global-mode +1))
     :config
     (progn
-      (setq sp-autoinsert-if-followed-by-word t)
       (setq sp-navigate-close-if-unbalanced t)
       (setq sp-message-width nil)
-
-      (add-hook 'prog-mode-hook 'smartparens-strict-mode)
-      (add-hook 'markdown-mode-hook 'smartparens-strict-mode)
-      (add-hook 'ielm-mode-hook 'smartparens-strict-mode)
 
       (add-hook 'minibuffer-setup-hook 'sp/maybe-enable-smartparens t)
       (add-hook 'minibuffer-inactive-mode-hook 'sp/maybe-enable-smartparens t)
 
       (add-hook 'smartparens-mode-hook 'sp/hacky-set-sp-bindings t)
-      (add-hook 'smartparens-strict-mode-hook 'sp/hacky-set-sp-bindings t))))
+      (add-hook 'smartparens-strict-mode-hook 'sp/hacky-set-sp-bindings t)
+
+      (smartparens-global-strict-mode +1)
+      (show-smartparens-global-mode +1))))
