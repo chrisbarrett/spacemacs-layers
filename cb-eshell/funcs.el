6ba4a0d683e0e98c6351289871875fd5f9fafda6
Chris Barrett <chris.d.barrett@me.com>
2015-07-11 10:28:11 +1200

    eshell: prompt shows project type

--- cb-eshell/funcs.el
+++ cb-eshell/funcs.el
@@ -68,18 +68,22 @@ With prefix argument ARG, always create a new shell."
   (require 'magit)
   (let ((prompt
          (list
-          :directory (abbreviate-file-name (eshell/pwd))
+          :directory (f-full (eshell/pwd))
           :root-user? (= (user-uid) 0)
-          :git-hash (magit-git-string "rev-parse" "HEAD")
-          :git-tag  (magit-get-current-tag)
-          :git-root (locate-dominating-file (eshell/pwd) ".git")
-          :git-branch (magit-get-current-branch)
-          :git-staged? (magit-anything-staged-p)
-          :git-unmerged? (magit-anything-unmerged-p)
-          :git-untracked? (ignore-errors
-                            (not (s-blank? (s-trim (shell-command-to-string "git ls-files --other --directory --exclude-standard")))))
-          :git-unstaged? (magit-anything-staged-p)
-          :git-modified? (magit-anything-modified-p)
+          :git (list
+                :hash (magit-git-string "rev-parse" "HEAD")
+                :tag  (magit-get-current-tag)
+                :root
+                (-when-let (root (locate-dominating-file (eshell/pwd) ".git"))
+                  (f-full root))
+                :branch (magit-get-current-branch)
+                :staged? (magit-anything-staged-p)
+                :unmerged? (magit-anything-unmerged-p)
+                :untracked? (ignore-errors
+                              (not (s-blank? (s-trim (shell-command-to-string "git ls-files --other --directory --exclude-standard")))))
+                :unstaged? (magit-anything-staged-p)
+                :modified? (magit-anything-modified-p))
+
           :last-command-success? (when (boundp 'eshell-last-command-status)
                                    (or (null eshell-last-command-status)
                                        (= eshell-last-command-status 0))))))
@@ -102,60 +106,146 @@ With prefix argument ARG, always create a new shell."
        (concat "\n\n" (cb-eshell--render-header plist) "\n")))
 
      (let ((colour (if last-command-success? solarized-hl-cyan solarized-hl-red)))
-       (propertize (concat "λ" (if root-user? "#" ">")) 'face `(:bold t :foreground ,colour)))
+       (concat
+        (propertize "λ" 'face `(:foreground ,colour))
+        (propertize (if root-user? "#" ">") 'face `(:foreground ,colour))))
 
      " ")))
 
 (defun cb-eshell--render-header (plist)
-  (-let [(&plist
-          :git-branch git-branch
-          :git-hash git-hash
-          :git-tag git-tag
-          :git-root git-root
-          :directory dir
-          :git-staged? git-staged?
-          :git-unstaged? git-unstaged?
-          :git-unmerged? git-unmerged?
-          :git-untracked? git-untracked?
-          :git-modified? git-modified?
-          ) plist]
-    (if git-hash
-        ;; Git repo info.
-        (let ((repo-subdirs (f-relative dir git-root)))
-          (concat (propertize (s-chop-suffix "/" git-root) 'face `(:foreground ,solarized-hl-blue))
-                  "\n" (propertize " - " 'face font-lock-comment-face)
+  (-let* (((&plist
+            :directory dir
+            :git
+            (&plist
+             :branch git-branch
+             :hash git-hash
+             :tag git-tag
+             :root git-root
+             :staged? git-staged?
+             :unstaged? git-unstaged?
+             :unmerged? git-unmerged?
+             :untracked? git-untracked?
+             :modified? git-modified?)
+            ) plist)
+          (sections (concat
+                     ;; Display project info when at project root.
+                     (when (equal git-root dir)
+                       (-when-let (parts (-non-nil
+                                          (list
+                                           (when git-hash
+                                             (concat (propertize "vc:" 'face font-lock-comment-face)
+                                                     (propertize "git" 'face 'default)))
+                                           (-when-let (lang (cb-eshell--project-lang git-root))
+                                             (concat
+                                              (propertize "lang:" 'face font-lock-comment-face)
+                                              (propertize lang 'face 'default)))
 
-                  (if git-branch
-                      (concat
-                       (propertize "branch:" 'face font-lock-comment-face)
-                       (propertize git-branch 'face 'magit-branch-remote))
-                    (propertize "DETACHED" 'face `(:foreground ,solarized-hl-orange)))
+                                           (-when-let (types (cb-eshell--project-types git-root))
+                                             (concat
+                                              (propertize "type:[" 'face font-lock-comment-face)
+                                              (propertize (s-join " " types) 'face 'default)
+                                              (propertize "]" 'face font-lock-comment-face)
+                                              ))
+                                           )))
+                         (cb-eshell--prompt-section
+                          (propertize "project " 'face font-lock-comment-face)
+                          (s-join " " parts))))
 
-                  (when git-tag
-                    (concat
-                     (propertize " tag:" 'face font-lock-comment-face)
-                     (propertize git-tag 'face 'magit-tag)))
+                     ;; Git branch info and status.
+                     (when git-root
+                       (cb-eshell--prompt-section
+                        (if git-branch
+                            (concat
+                             (propertize "branch:" 'face font-lock-comment-face)
+                             (propertize git-branch 'face 'magit-branch-remote))
+                          (propertize "DETACHED" 'face `(:foreground ,solarized-hl-orange)))
 
-                  (propertize " sha:" 'face font-lock-comment-face)
-                  (propertize (substring git-hash 0 6) 'face 'default)
+                        (when git-tag
+                          (concat
+                           (propertize " tag:" 'face font-lock-comment-face)
+                           (propertize git-tag 'face 'magit-tag)))
 
-                  (when (or git-staged? git-unstaged? git-modified?)
-                    (concat
-                     (propertize " state:{" 'face font-lock-comment-face)
-                     (s-join " "
-                             (-keep 'identity
-                                    (list
-                                     (when git-staged? (propertize "staged" 'face `(:foreground ,solarized-hl-green)))
-                                     (when git-unstaged? (propertize "unstaged" 'face `(:foreground ,solarized-hl-cyan)))
-                                     (when git-unmerged? (propertize "unmerged" 'face `(:foreground ,solarized-hl-magenta)))
-                                     (when git-modified? (propertize "modified" 'face `(:foreground ,solarized-hl-red)))
-                                     (when git-untracked? (propertize "untracked" 'face 'default)))))
-                     (propertize "}" 'face font-lock-comment-face)))
+                        (propertize " sha:" 'face font-lock-comment-face)
+                        (propertize (substring git-hash 0 6) 'face 'default)
 
-                  "\n"
-                  (propertize " - " 'face font-lock-comment-face)
-                  (propertize (concat "/" repo-subdirs) 'face `(:foreground ,solarized-hl-blue))
-                  "\n"))
+                        (-when-let (statuses
+                                    (-non-nil (list
+                                               (when git-staged? (propertize "staged" 'face `(:foreground ,solarized-hl-green)))
+                                               (when git-unstaged? (propertize "unstaged" 'face `(:foreground ,solarized-hl-cyan)))
+                                               (when git-unmerged? (propertize "unmerged" 'face `(:foreground ,solarized-hl-magenta)))
+                                               (when git-modified? (propertize "modified" 'face `(:foreground ,solarized-hl-red)))
+                                               (when git-untracked? (propertize "untracked" 'face 'default)))))
+                          (concat
+                           (propertize " state:[" 'face font-lock-comment-face)
+                           (s-join " " (-non-nil statuses))
+                           (propertize "]" 'face font-lock-comment-face)))))
 
-      ;; Plain dir.
-      (propertize dir 'face `(:bold t :foreground ,solarized-hl-blue)))))
+                     ;; Git project subdirectory
+                     (when git-hash
+                       (let ((repo-subdirs (f-relative dir git-root)))
+                         (cb-eshell--prompt-section
+                          (propertize (s-chop-prefix "/" (concat "/" repo-subdirs)) 'face `(:foreground ,solarized-hl-blue))))))))
+    (concat
+     ;; Current directory or git project root.
+     (propertize (f-abbrev (if git-hash (s-chop-suffix "/" git-root) dir)) 'face `(:foreground ,solarized-hl-blue))
+     sections
+     (unless (s-blank? sections) "\n"))))
+
+(defun cb-eshell--prompt-section (&rest components)
+  (concat "\n"
+          (propertize " - " 'face font-lock-comment-face)
+          (s-join "" components)))
+
+(defun cb-eshell--project-lang (project-root)
+  (let ((files (-map 'f-ext (f-files project-root))))
+    (cond
+     ((--any? (equal "cabal" it) files)
+      "haskell")
+     ((--any? (equal "hs" it) files)
+      "haskell")
+     ((--any? (equal "sbt" it) files)
+      "scala")
+     ((--any? (equal "scala" it) files)
+      "scala")
+     ((--any? (equal "el" it) files)
+      "elisp"))))
+
+(defun cb-eshell--project-types (project-root)
+  (let ((files (f-files project-root)))
+    (-non-nil
+     (list
+
+      (when (--any? (s-matches? (rx "/Cask" eos) it) files)
+        "cask")
+
+      (when (--any? (s-matches? (rx "/.travis.yml" eos) it) files)
+        "travis")
+
+      (when (--any? (s-matches? (rx "/.coveragerc" eos) it) files)
+        "coveralls")
+
+      (when (--any? (s-matches? (rx "/" (or "M" "m") "akefile" eos) it) files)
+        "make")
+
+      (when (--any? (s-matches? (rx ".cabal" eos) it) files)
+        "cabal")
+
+      ;; Scala projects
+
+      (let ((build-sbt (f-join default-directory "build.sbt")))
+        (-when-let ((_ scala-version)
+                    (and (f-exists? build-sbt)
+                         (s-match (rx bol (* space) "scalaVersion" (* space) ":=" (* space) "\"" (group (+ (any digit "."))))
+                                  (f-read-text build-sbt 'utf-8))))
+          scala-version))
+
+      (when (--any? (s-matches? (rx ".sbt" eos) it) files)
+        "sbt")
+
+      (let ((sbt-plugins (f-join project-root "project/plugins.sbt")))
+        (when (and (f-exists? sbt-plugins)
+                   (s-contains? "com.typesafe.play" (f-read-text sbt-plugins 'utf-8)))
+          "play"))
+
+      (when (--any? (s-matches? (rx "/init.el" eos) it) files)
+        "emacs.d")))))
