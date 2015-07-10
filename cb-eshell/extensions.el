4167220e1e8a7d7ec24f4c2250ab1a92b4d55587
Chris Barrett <chris.d.barrett@me.com>
2015-07-10 23:26:37 +1200

    eshell: basic setup

--- cb-eshell/extensions.el
+++ cb-eshell/extensions.el
@@ -7,10 +7,42 @@
 (defconst cb-eshell-post-extensions '())
 
 (eval-when-compile
+  (require 'f nil t)
   (require 'use-package nil t))
 
 (defun cb-eshell/init-eshell ()
   (use-package eshell
     :defer t
     :init
-    (global-set-key (kbd "<f1>") 'cb-eshell-bring)))
+    (progn
+      (setq eshell-directory-name (f-join user-layers-directory "cb-eshell/eshell/"))
+      (global-set-key (kbd "<f1>") 'cb-eshell-bring))
+
+    :config
+    (progn
+      (setq eshell-cmpl-ignore-case t)
+      (setq eshell-history-size 10000)
+      (setq eshell-hist-ignoredups t)
+      (setq eshell-error-if-no-glob t)
+      (setq eshell-glob-case-insensitive t)
+      (setq eshell-scroll-to-bottom-on-input t)
+
+      (with-eval-after-load 'em-prompt
+        (set-face-foreground 'eshell-prompt solarized-hl-cyan))
+
+      (evil-leader/set-key-for-mode 'eshell-mode "ib" 'eshell-insert-buffer-name)
+      (evil-leader/set-key-for-mode 'eshell-mode "ii" 'eshell-insert-process)
+      (evil-leader/set-key-for-mode 'eshell-mode "iv" 'eshell-insert-envvar)
+
+      (defun cb-eshell/set-keys ()
+        (local-set-key (kbd "C-c RET") 'eshell-toggle-direct-send))
+
+      (add-hook 'eshell-mode-hook 'cb-eshell/set-keys)
+      (add-hook 'eshell-mode-hook 'smartparens-strict-mode)
+
+      (defun pcomplete/sudo ()
+        (let ((prec (pcomplete-arg 'last -1)))
+          (cond ((string= "sudo" prec)
+                 (while (pcomplete-here*
+                         (funcall pcomplete-command-completion-function)
+                         (pcomplete-arg 'last) t)))))))))
