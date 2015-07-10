5a30e36e7691fa7533eeb86f44d2b7b61ae70c0b
Chris Barrett <chris.d.barrett@me.com>
2015-07-11 09:03:17 +1200

    eshell: disable eshell-prompt-extras

---
+++ cb-eshell/packages.el
@@ -0,0 +1,7 @@
+;;; packages.el --- Eshell packages  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(defconst cb-eshell-packages '())
+
+(defconst cb-eshell-excluded-packages '(eshell-prompt-extras))
