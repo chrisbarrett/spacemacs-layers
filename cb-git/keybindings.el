7ed56a51f12dad0c5355abbe9f58cb0148a011de
Chris Barrett <chris.d.barrett@me.com>
2015-07-28 11:17:19 +1200

    git: Update key bindings

--- cb-git/keybindings.el
+++ cb-git/keybindings.el
@@ -5,8 +5,8 @@
 (evil-leader/set-key "gD" 'magit-diff-working-tree)
 (evil-leader/set-key "gB" 'magit-blame)
 (evil-leader/set-key "gb" 'magit-branch-popup)
-(evil-leader/set-key "gn" 'git-gutter:next-hunk)
-(evil-leader/set-key "gp" 'git-gutter:previous-hunk)
+(evil-leader/set-key "gn" 'diff-hl-next-hunk)
+(evil-leader/set-key "gp" 'diff-hl-previous-hunk)
 
 (evil-set-initial-state 'git-commit-mode 'insert)
 
@@ -21,13 +21,11 @@
 (evil-global-set-key 'normal (kbd "g c") 'magit-commit-popup)
 (evil-global-set-key 'normal (kbd "g l") 'magit-log-popup)
 (evil-global-set-key 'normal (kbd "g a") 'git/add-this-file)
-(evil-global-set-key 'normal (kbd "g s") 'git-gutter:stage-hunk)
-(evil-global-set-key 'normal (kbd "g r") 'git-gutter:revert-hunk)
-(evil-global-set-key 'normal (kbd "g n") 'git-gutter:next-hunk)
-(evil-global-set-key 'normal (kbd "g p") 'git-gutter:previous-hunk)
+(evil-global-set-key 'normal (kbd "g n") 'diff-hl-next-hunk)
+(evil-global-set-key 'normal (kbd "g p") 'diff-hl-previous-hunk)
 (evil-global-set-key 'normal (kbd "g P") 'magit-push-popup)
 
 (with-eval-after-load 'magit
-  (define-key magit-mode-map (kbd "C-k") 'magit-copy-item-as-kill)
-  (define-key magit-mode-map (kbd "C-y") 'magit-copy-item-as-kill)
+  (define-key magit-mode-map (kbd "C-k") 'magit-copy-as-kill)
+  (define-key magit-mode-map (kbd "C-y") 'magit-copy-as-kill)
   )
