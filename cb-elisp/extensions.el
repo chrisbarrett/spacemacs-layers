3ef7059cd2f1a9f0e14cb78520981bc160e7bea4
Chris Barrett <chris.d.barrett@me.com>
2015-08-07 08:48:06 +1200

    elisp: update smart ops

--- cb-elisp/extensions.el
+++ cb-elisp/extensions.el
@@ -13,6 +13,6 @@
   (use-package smart-ops
     :config
     (define-smart-ops-for-mode 'emacs-lisp-mode
-      (smart-op "," :pad-before? t :pad-after nil)
-      (smart-op ",@" :pad-before t :pad-after? nil)
-      (smart-op "." :pad-before? t :pad-after? t))))
+      (smart-ops "." ",@" "," :pad-before t :pad-after nil))))
+
+;;; extensions.el ends here
