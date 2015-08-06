cf9b243c07ce7bcd03ac6ccca203df65240d96a1
Chris Barrett <chris.d.barrett@me.com>
2015-08-07 09:08:34 +1200

    groovy: update smart-ops

--- cb-groovy/extensions.el
+++ cb-groovy/extensions.el
@@ -5,22 +5,18 @@
 (defconst cb-groovy-pre-extensions '()
   "List of all extensions to load before the packages.")
 
-(defconst cb-groovy-post-extensions '(super-smart-ops)
+(defconst cb-groovy-post-extensions '(smart-ops)
   "List of all extensions to load after the packages.")
 
 (eval-when-compile
   (require 'use-package nil t))
 
-(defun cb-groovy/init-super-smart-ops ()
-  (use-package super-smart-ops
+(defun cb-groovy/init-smart-ops ()
+  (use-package smart-ops
     :config
     (progn
-      (super-smart-ops-configure-for-mode 'groovy-mode
-        :custom
-        `((":" . ,(super-smart-ops-make-smart-op ":" nil t))
-          ("," . ,(super-smart-ops-make-smart-op "," nil t))))
+      (define-smart-ops-for-mode 'groovy-mode
+        (smart-ops ":" "," :pad-before nil))
 
-      (super-smart-ops-configure-for-mode 'inferior-groovy-mode
-        :custom
-        `((":" . ,(super-smart-ops-make-smart-op ":" nil t))
-          ("," . ,(super-smart-ops-make-smart-op "," nil t)))))))
+      (define-smart-ops-for-mode 'inferior-groovy-mode
+        (smart-ops ":" "," :pad-before nil)))))
