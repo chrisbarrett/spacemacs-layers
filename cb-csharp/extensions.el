0b052a3fede733dc7f5a8533a0373be1964e3d0c
Chris Barrett <chris.d.barrett@me.com>
2015-08-08 13:10:25 +1200

    csharp: use new smart ops

--- cb-csharp/extensions.el
+++ cb-csharp/extensions.el
@@ -7,17 +7,15 @@
   "List of all extensions to load before the packages.")
 
 (defconst cb-csharp-post-extensions
-  '(super-smart-ops)
+  '(smart-ops)
   "List of all extensions to load after the packages.")
 
 (eval-when-compile
   (require 'use-package nil t))
 
-(defun cb-csharp/init-super-smart-ops ()
-  (use-package super-smart-ops
+(defun cb-csharp/init-smart-ops ()
+  (use-package smart-ops
     :config
-    (super-smart-ops-configure-for-mode 'csharp-mode
-      :add '("?")
-      :custom
-      `(("," . ,(super-smart-ops-make-smart-op "," nil t))
-        (";" . ,(super-smart-ops-make-smart-op ";" nil t))))))
+    (define-smart-ops-for-mode 'csharp-mode
+      (smart-ops "," ";" :pad-before nil :pad-after t)
+      (smart-ops-default-ops))))
