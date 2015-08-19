2cecddb9f67a858000e9d25aee71aef661e96302
Chris Barrett <chris.d.barrett@me.com>
2015-08-19 19:49:57 +1200

    flycheck: remove haskell-process checker

--- cb-flycheck/extensions.el
+++ cb-flycheck/extensions.el
@@ -7,21 +7,8 @@
   "List of all extensions to load before the packages.")
 
 (defconst cb-flycheck-post-extensions
-  '(haskell-flycheck)
+  '()
   "List of all extensions to load after the packages.")
 
 (eval-when-compile
   (require 'use-package nil t))
-
-(defun cb-flycheck/init-haskell-flycheck ()
-  (use-package haskell-flycheck
-    :config
-    (progn
-      ;;; Use the Haskell session for syntax checking when available
-
-      (defun cb-flycheck/configure-haskell-checker ()
-        (flycheck-select-checker 'haskell-process)
-        (flycheck-mode +1))
-
-      (add-hook 'interactive-haskell-mode-hook 'cb-flycheck/configure-haskell-checker)
-      )))
