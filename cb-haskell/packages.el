ce088d6da3d1cdb9ca7914f44162aa923b572344
Chris Barrett <chris.d.barrett@me.com>
2015-08-19 18:55:48 +1200

    haskell: remove haskell-snippets

--- cb-haskell/packages.el
+++ cb-haskell/packages.el
@@ -14,7 +14,7 @@
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defconst cb-haskell-excluded-packages '()
+(defconst cb-haskell-excluded-packages '(haskell-snippets)
   "List of packages to exclude.")
 
 (eval-when-compile
