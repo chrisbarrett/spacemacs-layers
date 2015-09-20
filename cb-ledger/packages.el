72fb6cfa93977702f7580eb000882ea236df17bb
Chris Barrett <chris.d.barrett@me.com>
2015-09-21 09:17:41 +1200

    ledger: improve reports
    - Shell out to ledger to find pay periods accurately
    - Use format specifiers to simplify implementation

--- cb-ledger/packages.el
+++ cb-ledger/packages.el
@@ -25,61 +25,53 @@ which require an initialization must be listed explicitly in the list.")
       (setq ledger-post-use-completion-engine :ido)
       (setq ledger-fontify-xact-state-overrides nil)
 
-      (defconst cb-ledger/pay-day-of-month 13)
+      (defconst cb-ledger/income-account-name "Income:Movio")
 
-      (defun cb-ledger/last-payday-at (date)
-        (-let* (((s minute hour day month year) (decode-time date))
-                (pay-day-of-month cb-ledger/pay-day-of-month)
-                (before-pay-day? (< day pay-day-of-month))
-                ((pay-month pay-year)
-                 (cond
-                  ((and (= 1 month) before-pay-day?)
-                   (list 12 (1- year)))
-                  (before-pay-day?
-                   (list (1- month) year))
-                  (t
-                   (list month year)))))
-          (encode-time s minute hour pay-day-of-month pay-month pay-year)))
+      (defun cb-ledger/last-n-pay-dates (n)
+        (let ((output (shell-command-to-string
+                       (format "ledger -f '%s' reg '%s' -y '%%Y-%%m-%%d' | tail -n %s | awk '{print $1}'"
+                               ledger-master-file
+                               cb-ledger/income-account-name
+                               n))))
+          (s-split "\n" output t)))
 
-      (setq ledger-reports
-            (let* ((prev-payday-time (cb-ledger/last-payday-at (current-time)))
-                   (time-before-last-payday (time-subtract prev-payday-time (days-to-time 1)))
-                   (second-prev-payday-time (cb-ledger/last-payday-at time-before-last-payday))
+      (defun cb-ledger/last-payday ()
+        (car (cb-ledger/last-n-pay-dates 1)))
 
-                   (prev-payday (format-time-string "%Y-%m-%d" prev-payday-time))
-                   (day-before-last-payday (format-time-string "%Y-%m-%d" time-before-last-payday))
-                   (second-prev-payday (format-time-string "%Y-%m-%d" second-prev-payday-time))
-                   (last-pay-period-expr (format "from %s to %s" second-prev-payday day-before-last-payday)))
+      (defun cb-ledger/previous-pay-period ()
+        (-let [(prev cur) (cb-ledger/last-n-pay-dates 2)]
+          (format "from %s to %s" prev (cb-ledger/day-before cur))))
 
-              `(("assets" "ledger -f %(ledger-file) bal assets")
-                ("balance" "ledger -f %(ledger-file) bal")
-                ("register" "ledger -f %(ledger-file) reg")
-                ("payee" "ledger -f %(ledger-file) reg @%(payee)")
-                ("account" "ledger -f %(ledger-file) reg %(account)")
-                ("net worth" "ledger -f %(ledger-file) bal ^assets ^liabilities")
-                ("cash flow" "ledger -f %(ledger-file) bal ^income ^expenses")
-                ("this week" "ledger -f %(ledger-file) -p 'this week' -r reg 'checking' --invert")
-                ("this month" "ledger -f %(ledger-file) -p 'this month' -r reg 'checking' --invert")
-                ("reg since payday"
-                 ,(concat "ledger -f %(ledger-file) -b '" prev-payday
-                          "' -r reg 'checking' --invert"))
-                ("bal since payday"
-                 ,(concat "ledger -f %(ledger-file) -b '" prev-payday
-                          "' -r bal 'checking' --invert"))
-
-                ("reg last pay period"
-                 ,(concat "ledger -f %(ledger-file) -p '" last-pay-period-expr
-                          "' -r reg 'checking' --invert"))
-                ("bal last pay period"
-                 ,(concat "ledger -f %(ledger-file) -p '" last-pay-period-expr
-                          "' -r bal 'checking' --invert")))))
+      (defun cb-ledger/day-before (ledger-date-str)
+        (-let* (((y m d) (-map 'string-to-number (s-split "[/-]" ledger-date-str)))
+                (date (encode-time 0 0 0 d m y))
+                (updated (time-subtract date (days-to-time 1))))
+          (format-time-string "%Y-%m-%d" updated)))
 
       (setq ledger-report-format-specifiers
             '(("ledger-file" . ledger-report-ledger-file-format-specifier)
+              ("last-payday" . cb-ledger/last-payday)
+              ("prev-pay-period" . cb-ledger/previous-pay-period)
               ("payee" . ledger-report-payee-format-specifier)
               ("account" . ledger-report-account-format-specifier)
               ("tagname" . ledger-report-tagname-format-specifier)
               ("tagvalue" . ledger-report-tagvalue-format-specifier)))
+
+      (setq ledger-reports
+            '(("assets" "ledger -f %(ledger-file) bal assets")
+              ("balance" "ledger -f %(ledger-file) bal")
+              ("register" "ledger -f %(ledger-file) reg")
+              ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+              ("account" "ledger -f %(ledger-file) reg %(account)")
+              ("net worth" "ledger -f %(ledger-file) bal ^assets ^liabilities")
+              ("cash flow" "ledger -f %(ledger-file) bal ^income ^expenses")
+              ("this week" "ledger -f %(ledger-file) -p 'this week' -r reg 'checking' --invert")
+              ("this month" "ledger -f %(ledger-file) -p 'this month' -r reg 'checking' --invert")
+              ("reg since payday" "ledger -f %(ledger-file) -b %(last-payday) -r reg 'checking' --invert")
+              ("bal since payday" "ledger -f %(ledger-file) -b %(last-payday) -r bal 'checking' --invert")
+
+              ("reg last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r reg 'checking' --invert")
+              ("bal last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r bal 'checking' --invert")))
 
       (custom-set-faces
        '(ledger-occur-xact-face
