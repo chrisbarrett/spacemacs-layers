d8c0835a810a6fb29902ad532fd88fac48fce866
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:30:54 +1300

    port cb-org-reveal

--- cb-org-reveal/packages.el
+++ cb-org-reveal/packages.el
@@ -1,17 +1,15 @@
 ;;; packages.el --- cb-org-reveal Layer packages File for Spacemacs
 ;;; Code:
 
-(defconst cb-org-reveal-packages '(ox-reveal))
-
-(defconst cb-org-reveal-excluded-packages '())
-
 (eval-when-compile
   (require 'use-package nil t))
+
+(defconst cb-org-reveal-packages '(ox-reveal))
 
 (defun cb-org-reveal/init-ox-reveal ()
   (use-package ox-reveal
     :config
-    (let ((base (f-dirname (symbol-file 'cb-org-reveal-excluded-packages))))
+    (let ((base (f-dirname (symbol-file 'cb-org-reveal-packages))))
       (setq org-reveal-root (f-join base "submodules" "reveal.js")))))
 
 ;;; End
