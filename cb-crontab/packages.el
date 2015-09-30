ada1539c8c94b62efbb4de65e056bf36c121e9d5
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:01:16 +1300

    update cb-crontab

--- cb-crontab/packages.el
+++ cb-crontab/packages.el
@@ -2,13 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-crontab-packages
-  '(crontab-mode))
-
-(defconst cb-crontab-excluded-packages '())
-
 (eval-when-compile
   (require 'use-package nil t))
+
+(defconst cb-crontab-packages
+  '(crontab-mode))
 
 (defun cb-crontab/init-crontab-mode ()
   (use-package crontab-mode
