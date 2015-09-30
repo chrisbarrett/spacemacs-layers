61c0faf6505e6546a7a45baf2cb663d3a20cc0bd
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:07:22 +1300

    port cb-sql

---
+++ cb-sql/packages.el
@@ -0,0 +1,19 @@
+;;; extensions.el --- cb-sql Layer packages File for Spacemacs
+;;; Commentary:
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package nil t))
+
+(defconst cb-sql-packages
+  '(aggressive-indent
+    sql))
+
+(defun cb-sql/post-init-aggressive-indent ()
+  (with-eval-after-load 'aggressive-indent
+    (add-to-list 'aggressive-indent-excluded-modes 'sql-interactive-mode)
+    (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)))
+
+(defun cb-sql/init-sql ()
+  (with-eval-after-load 'sql
+    (define-key sql-mode-map (kbd "C-c C-z") 'sql-product-interactive)))
