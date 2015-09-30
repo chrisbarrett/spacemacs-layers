24554e4213ec5bd013e9a344b028046610cc52d4
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:41:54 +1300

    port cb-csharp

--- cb-csharp/packages.el
+++ cb-csharp/packages.el
@@ -2,24 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-csharp-packages
-  '(omnisharp)
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
-
-(defconst cb-csharp-excluded-packages '()
-  "List of packages to exclude.")
-
 (eval-when-compile
   (require 'use-package nil t))
 
-(defun cb-csharp/init-omnisharp ()
-  (use-package omnisharp
-    :commands omnisharp-mode
-    :diminish "O#"
+(defconst cb-csharp-packages
+  '(omnisharp
+    smart-ops))
+
+(defun cb-csharp/post-init-omnisharp ()
+  (diminish 'omnisharp-mode "O#")
+  (add-hook 'csharp-mode-hook 'omnisharp-mode)
+  (add-hook 'csharp-mode-hook 'eldoc-mode)
+  (setq omnisharp-server-executable-path "~/src/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp.exe")
+  (setq omnisharp-auto-complete-want-documentation nil))
+
+(defun cb-csharp/post-init-smart-ops ()
+  (use-package smart-ops
     :config
-    (progn
-      (add-hook 'csharp-mode-hook 'omnisharp-mode)
-      (add-hook 'csharp-mode-hook 'eldoc-mode)
-      (setq omnisharp-server-executable-path "~/src/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp.exe")
-      (setq omnisharp-auto-complete-want-documentation nil))))
+    (define-smart-ops-for-mode 'csharp-mode
+      (smart-ops "," ";" :pad-before nil :pad-after t)
+      (smart-ops-default-ops))))
