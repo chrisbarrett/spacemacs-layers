d5de76c1b9dd8dd4a7c9025f07ad77873d57a48b
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 19:56:28 +1300

    port cb-ledger

--- cb-ledger/packages.el
+++ cb-ledger/packages.el
@@ -14,79 +14,76 @@ which require an initialization must be listed explicitly in the list.")
   (require 'dash nil t)
   (require 'use-package nil t))
 
-(defun cb-ledger/init-ledger-mode ()
-  (use-package ledger-mode
-    :init
-    (add-to-list 'auto-mode-alist (cons "\\.ledger$" 'ledger-mode))
-    :config
-    (progn
-      (setq ledger-master-file (f-join org-directory "accounts.ledger"))
-      (setq ledger-post-account-alignment-column 2)
-      (setq ledger-post-use-completion-engine :ido)
-      (setq ledger-fontify-xact-state-overrides nil)
+(defun cb-ledger/post-init-ledger-mode ()
+  (add-to-list 'auto-mode-alist (cons "\\.ledger$" 'ledger-mode))
 
-      (defconst cb-ledger/income-account-name "Income:Movio")
+  (setq ledger-master-file (f-join org-directory "accounts.ledger"))
+  (setq ledger-post-account-alignment-column 2)
+  (setq ledger-post-use-completion-engine :ido)
+  (setq ledger-fontify-xact-state-overrides nil)
 
-      (defun cb-ledger/last-n-pay-dates (n)
-        (let ((output (shell-command-to-string
-                       (format "ledger -f '%s' reg '%s' -y '%%Y-%%m-%%d' | tail -n %s | awk '{print $1}'"
-                               ledger-master-file
-                               cb-ledger/income-account-name
-                               n))))
-          (s-split "\n" output t)))
+  (defconst cb-ledger/income-account-name "Income:Movio")
 
-      (defun cb-ledger/last-payday ()
-        (car (cb-ledger/last-n-pay-dates 1)))
+  (defun cb-ledger/last-n-pay-dates (n)
+    (let ((output (shell-command-to-string
+                   (format "ledger -f '%s' reg '%s' -y '%%Y-%%m-%%d' | tail -n %s | awk '{print $1}'"
+                           ledger-master-file
+                           cb-ledger/income-account-name
+                           n))))
+      (s-split "\n" output t)))
 
-      (defun cb-ledger/previous-pay-period ()
-        (-let [(prev cur) (cb-ledger/last-n-pay-dates 2)]
-          (format "from %s to %s" prev (cb-ledger/day-before cur))))
+  (defun cb-ledger/last-payday ()
+    (car (cb-ledger/last-n-pay-dates 1)))
 
-      (defun cb-ledger/day-before (ledger-date-str)
-        (-let* (((y m d) (-map 'string-to-number (s-split "[/-]" ledger-date-str)))
-                (date (encode-time 0 0 0 d m y))
-                (updated (time-subtract date (days-to-time 1))))
-          (format-time-string "%Y-%m-%d" updated)))
+  (defun cb-ledger/previous-pay-period ()
+    (-let [(prev cur) (cb-ledger/last-n-pay-dates 2)]
+      (format "from %s to %s" prev (cb-ledger/day-before cur))))
 
-      (setq ledger-report-format-specifiers
-            '(("ledger-file" . ledger-report-ledger-file-format-specifier)
-              ("last-payday" . cb-ledger/last-payday)
-              ("prev-pay-period" . cb-ledger/previous-pay-period)
-              ("payee" . ledger-report-payee-format-specifier)
-              ("account" . ledger-report-account-format-specifier)
-              ("tagname" . ledger-report-tagname-format-specifier)
-              ("tagvalue" . ledger-report-tagvalue-format-specifier)))
+  (defun cb-ledger/day-before (ledger-date-str)
+    (-let* (((y m d) (-map 'string-to-number (s-split "[/-]" ledger-date-str)))
+            (date (encode-time 0 0 0 d m y))
+            (updated (time-subtract date (days-to-time 1))))
+      (format-time-string "%Y-%m-%d" updated)))
 
-      (setq ledger-reports
-            '(("assets" "ledger -f %(ledger-file) bal assets")
-              ("balance" "ledger -f %(ledger-file) bal")
-              ("register" "ledger -f %(ledger-file) reg")
-              ("payee" "ledger -f %(ledger-file) reg @%(payee)")
-              ("account" "ledger -f %(ledger-file) reg %(account)")
-              ("net worth" "ledger -f %(ledger-file) bal ^assets ^liabilities")
-              ("cash flow" "ledger -f %(ledger-file) bal ^income ^expenses")
-              ("this week" "ledger -f %(ledger-file) -p 'this week' -r reg 'checking' --invert")
-              ("this month" "ledger -f %(ledger-file) -p 'this month' -r reg 'checking' --invert")
-              ("reg since payday" "ledger -f %(ledger-file) -b %(last-payday) -r reg 'checking' --invert")
-              ("bal since payday" "ledger -f %(ledger-file) -b %(last-payday) -r bal 'checking' --invert")
+  (setq ledger-report-format-specifiers
+        '(("ledger-file" . ledger-report-ledger-file-format-specifier)
+          ("last-payday" . cb-ledger/last-payday)
+          ("prev-pay-period" . cb-ledger/previous-pay-period)
+          ("payee" . ledger-report-payee-format-specifier)
+          ("account" . ledger-report-account-format-specifier)
+          ("tagname" . ledger-report-tagname-format-specifier)
+          ("tagvalue" . ledger-report-tagvalue-format-specifier)))
 
-              ("reg last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r reg 'checking' --invert")
-              ("bal last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r bal 'checking' --invert")))
+  (setq ledger-reports
+        '(("assets" "ledger -f %(ledger-file) bal assets")
+          ("balance" "ledger -f %(ledger-file) bal")
+          ("register" "ledger -f %(ledger-file) reg")
+          ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+          ("account" "ledger -f %(ledger-file) reg %(account)")
+          ("net worth" "ledger -f %(ledger-file) bal ^assets ^liabilities")
+          ("cash flow" "ledger -f %(ledger-file) bal ^income ^expenses")
+          ("this week" "ledger -f %(ledger-file) -p 'this week' -r reg 'checking' --invert")
+          ("this month" "ledger -f %(ledger-file) -p 'this month' -r reg 'checking' --invert")
+          ("reg since payday" "ledger -f %(ledger-file) -b %(last-payday) -r reg 'checking' --invert")
+          ("bal since payday" "ledger -f %(ledger-file) -b %(last-payday) -r bal 'checking' --invert")
 
-      (custom-set-faces
-       '(ledger-occur-xact-face
-         ((((background dark))  :background "#073642")
-          (((background light)) :background "#eee8d5")))
-       `(ledger-font-pending-face
-         ((t (:foreground ,solarized-hl-orange))))
-       `(ledger-font-payee-cleared-face
-         ((t (:foreground ,solarized-hl-green))))
-       `(ledger-font-payee-uncleared-face
-         ((t (:foreground ,solarized-hl-orange))))
-       `(ledger-font-posting-account-face
-         ((t (:foreground ,solarized-hl-blue)))))
+          ("reg last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r reg 'checking' --invert")
+          ("bal last pay period" "ledger -f %(ledger-file) -p %(prev-pay-period) -r bal 'checking' --invert")))
 
-      (core/remap-face 'ledger-font-comment-face 'font-lock-comment-face)
+  (custom-set-faces
+   '(ledger-occur-xact-face
+     ((((background dark))  :background "#073642")
+      (((background light)) :background "#eee8d5")))
+   `(ledger-font-pending-face
+     ((t (:foreground ,solarized-hl-orange))))
+   `(ledger-font-payee-cleared-face
+     ((t (:foreground ,solarized-hl-green))))
+   `(ledger-font-payee-uncleared-face
+     ((t (:foreground ,solarized-hl-orange))))
+   `(ledger-font-posting-account-face
+     ((t (:foreground ,solarized-hl-blue)))))
 
-      ;; Fix font lock issue in ledger reports
-      (add-hook 'ledger-report-mode-hook 'font-lock-fontify-buffer))))
+  (core/remap-face 'ledger-font-comment-face 'font-lock-comment-face)
+
+  ;; Fix font lock issue in ledger reports
+  (add-hook 'ledger-report-mode-hook 'font-lock-fontify-buffer))
