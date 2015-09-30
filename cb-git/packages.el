7084c8b6017064dfa6f3cf7377b5e7f9f9659b60
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:15:30 +1300

    port cb-git

--- cb-git/packages.el
+++ cb-git/packages.el
@@ -2,35 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
+(eval-when-compile
+  (require 'use-package nil t))
+
 (defconst cb-git-packages
-  '(
-    magit
-    ido-completing-read+
-    git-auto-commit-mode
-    )
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
+  '(magit
+    git-auto-commit-mode))
 
-(defconst cb-git-excluded-packages
-  '(git-gutter git-gutter-fringe)
-  "List of packages to exclude.")
+(defun cb-git/post-init-magit ()
+  ;; Remove broken Spacemacs customisation
+  (remove-hook 'git-commit-mode-hook 'fci-mode)
 
-(eval-when-compile
-  (require 'use-package nil t))
-
-(defun cb-git/init-magit ()
-  (use-package magit
-    :defer t
-    :config
-    (progn
-      ;; Remove broken Spacemacs customisation
-      (remove-hook 'git-commit-mode-hook 'fci-mode)
-
-      (core/remap-face 'magit-section-highlight 'core/bg-hl-ok)
-      (core/remap-face 'magit-diff-context-highlight 'core/bg-hl-ok))))
-
-(eval-when-compile
-  (require 'use-package nil t))
+  (core/remap-face 'magit-section-highlight 'core/bg-hl-ok)
+  (core/remap-face 'magit-diff-context-highlight 'core/bg-hl-ok))
 
 (defun cb-git/init-git-auto-commit-mode ()
   (use-package git-auto-commit-mode
