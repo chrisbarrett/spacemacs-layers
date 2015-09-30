44af1ba6e260a890ba80a424283b40578cacf24e
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 19:27:22 +1300

    port cb-flycheck

--- cb-flycheck/packages.el
+++ cb-flycheck/packages.el
@@ -2,22 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-flycheck-packages
-  '(flycheck)
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
-
-(defconst cb-flycheck-excluded-packages '(flycheck-pos-tip)
-  "List of packages to exclude.")
-
 (eval-when-compile
   (require 'use-package nil t))
 
-(defun cb-flycheck/init-flycheck ()
-  (use-package flycheck
-    :config
-    (progn
-      (setq flycheck-display-errors-function 'flycheck-display-error-messages-unless-error-list)
-      (setq flycheck-display-errors-delay 0)
-      (setq flycheck-check-syntax-automatically '(mode-enabled idle-change save))
-      (global-flycheck-mode +1))))
+(defconst cb-flycheck-packages
+  '(flycheck
+    (flycheck-pos-tip :excluded t)))
+
+(defun cb-flycheck/post-init-flycheck ()
+  (setq flycheck-display-errors-function 'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-display-errors-delay 0)
+  (setq flycheck-check-syntax-automatically '(mode-enabled idle-change save))
+  (global-flycheck-mode +1))
