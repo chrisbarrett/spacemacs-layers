9053b6b553c874990c7fa65c7685cfe38a3569dd
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:00:56 +1300

    update cb-js

--- cb-js/packages.el
+++ cb-js/packages.el
@@ -2,16 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-js-packages
-  '(js2-mode)
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
-
-(defconst cb-js-excluded-packages '()
-  "List of packages to exclude.")
-
 (eval-when-compile
   (require 'use-package nil t))
+
+(defconst cb-js-packages
+  '(js2-mode
+    js
+    smart-ops))
 
 (defun cb-js/init-js2-mode ()
   (use-package js2-mode
@@ -28,3 +25,18 @@ which require an initialization must be listed explicitly in the list.")
       ;; Use flycheck for checking.
       (setq js2-mode-show-parse-errors nil)
       (setq js2-mode-show-strict-warnings nil))))
+
+(defun cb-js/post-init-js ()
+  (use-package js
+    :defer t
+    :config
+    (setq js-indent-level 2)))
+
+(defun cb-js/post-init-smart-ops ()
+  (define-smart-ops-for-mode 'js-mode
+    (smart-ops ";" ":" "," :pad-before nil)
+    (smart-ops-default-ops))
+
+  (define-smart-ops-for-mode 'js2-mode
+    (smart-ops ";" ":" "," :pad-before nil)
+    (smart-ops-default-ops)))
