218ca62de4d2729568ee59555e8e4706d7ec2986
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 20:05:44 +1300

    port cb-eshell

--- cb-eshell/packages.el
+++ cb-eshell/packages.el
@@ -2,6 +2,62 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-eshell-packages '())
+(eval-when-compile
+  (require 'use-package nil t)
+  (require 'f nil t))
 
-(defconst cb-eshell-excluded-packages '(eshell-prompt-extras))
+(defconst cb-eshell-packages
+  '(eshell
+    '(eshell-prompt-extras :excluded t)))
+
+;; Disable company for eshell.
+(with-eval-after-load 'company
+  (setq company-global-modes '(not eshell-mode)))
+
+(defun cb-eshell/post-init-eshell ()
+  (setq eshell-directory-name (f-join user-dropbox-directory "emacs/eshell/"))
+  (global-set-key (kbd "<f1>") 'cb-eshell-bring)
+
+  (setq eshell-buffer-shorthand t)
+  (setq eshell-cmpl-ignore-case t)
+  (setq eshell-history-size 10000)
+  (setq eshell-hist-ignoredups t)
+  (setq eshell-error-if-no-glob t)
+  (setq eshell-glob-case-insensitive t)
+  (setq eshell-scroll-to-bottom-on-input t)
+  (setq eshell-prompt-function 'cb-eshell--prompt)
+
+  ;; See ./funcs.el for definition.
+  (setq eshell-prompt-regexp (rx bol (or
+                                      ;; default
+                                      (and (* space) "λ" (or ">" "#") " ")
+                                      ;; boot2docker
+                                      (and "@" (+ nonl) ":" (+ nonl) "$" space))))
+
+  (evil-set-initial-state 'eshell-mode 'insert)
+
+  (evil-leader/set-key-for-mode 'eshell-mode "ib" 'eshell-insert-buffer-name)
+  (evil-leader/set-key-for-mode 'eshell-mode "ii" 'eshell-insert-process)
+  (evil-leader/set-key-for-mode 'eshell-mode "iv" 'eshell-insert-envvar)
+
+  (defun cb-eshell/ret ()
+    "Do not send input if the command is empty."
+    (interactive)
+    (let ((empty-command? (s-matches? (concat eshell-prompt-regexp " *$") (current-line))))
+      (unless empty-command?
+        (call-interactively 'eshell-send-input))))
+
+  (defun cb-eshell/setup ()
+    (vi-tilde-fringe-mode -1)
+    (local-set-key (kbd "RET") 'cb-eshell/ret)
+    (local-set-key (kbd "C-c RET") 'eshell-toggle-direct-send))
+
+  (add-hook 'eshell-mode-hook 'cb-eshell/setup)
+  (add-hook 'eshell-mode-hook 'smartparens-strict-mode)
+
+  (defun pcomplete/sudo ()
+    (let ((prec (pcomplete-arg 'last -1)))
+      (cond ((string= "sudo" prec)
+             (while (pcomplete-here*
+                     (funcall pcomplete-command-completion-function)
+                     (pcomplete-arg 'last) t)))))))
