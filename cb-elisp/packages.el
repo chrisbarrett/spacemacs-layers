3a6729b54a92e3fc7f155581b2b41229e881366c
Chris Barrett <chris.d.barrett@me.com>
2015-09-30 19:32:52 +1300

    port cb-elisp layer

--- cb-elisp/packages.el
+++ cb-elisp/packages.el
@@ -2,82 +2,38 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-elisp-packages
-  '(
-    elisp-slime-nav
-    cl-lib-highlight
-    hl-sexp
-    highlight-defined
-    paredit
-    eval-sexp-fu
-    eldoc
-    dash
-    flycheck-cask
-    )
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
-
-(defconst cb-elisp-excluded-packages '()
-  "List of packages to exclude.")
-
 (eval-when-compile
   (require 'use-package nil t)
-  (require 's nil t))
+  (require 's nil t)
+  (require 'dash nil t))
 
-(defun cb-elisp/init-eldoc ()
-  (use-package eldoc
-    :diminish eldoc-mode
-    :commands eldoc-mode
-    :init
-    (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)))
+(defconst cb-elisp-packages
+  '(eldoc
+    eval-sexp-fu
+    dash
+    hl-sexp
+    highlight-defined
+    smart-ops
+    flycheck-cask))
 
-(defun cb-elisp/init-cl-lib-highlight ()
-  (use-package cl-lib-highlight
-    :commands (cl-lib-highlight-initialize
-               cl-lib-highlight-warn-cl-initialize)
-    :defer t
-    :init
-    (add-hook 'emacs-lisp-mode-hook 'cl-lib-highlight-initialize)
-    (add-hook 'emacs-lisp-mode-hook 'cl-lib-highlight-warn-cl-initialize)))
+(defun cb-elisp/post-init-eldoc ()
+  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
 
-(defun cb-elisp/init-paredit ()
-  (use-package paredit
-    :commands paredit-mode
-    :defer t
-    :init
-    (add-hook 'minibuffer-setup-hook
-              (lambda ()
-               (when (equal this-command 'eval-expression)
-                 (paredit-mode +1))))))
+(defun cb-elisp/post-init-eval-sexp-fu ()
+  (defun cb-elisp/configure-eval-sexp-fu ()
+    (core/remap-face 'eval-sexp-fu-flash 'core/bg-flash)
+    (core/remap-face 'eval-sexp-fu-flash-error 'core/bg-flash-red)
+    (turn-on-eval-sexp-fu-flash-mode))
 
-(defun cb-elisp/init-elisp-slime-nav ()
-  (use-package elisp-slime-nav
-    :commands elisp-slime-nav-mode
-    :init
-    (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)))
+  (add-hook 'emacs-lisp-mode-hook 'cb-elisp/configure-eval-sexp-fu)
 
-(defun cb-elisp/init-eval-sexp-fu ()
-  (use-package eval-sexp-fu
-    :commands turn-on-eval-sexp-fu-flash-mode
-    :init
-    (progn
-      (defun cb-elisp/configure-eval-sexp-fu ()
-        (core/remap-face 'eval-sexp-fu-flash 'core/bg-flash)
-        (core/remap-face 'eval-sexp-fu-flash-error 'core/bg-flash-red)
-        (turn-on-eval-sexp-fu-flash-mode))
+  (define-eval-sexp-fu-flash-command elisp/eval-dwim
+    (eval-sexp-fu-flash
+     (-let [(&plist :beg beg :end end) (elisp/thing-for-eval)]
+       (cons beg end)))))
 
-      (add-hook 'emacs-lisp-mode-hook 'cb-elisp/configure-eval-sexp-fu))
-
-    :config
-    (progn
-      (define-eval-sexp-fu-flash-command elisp/eval-dwim
-        (eval-sexp-fu-flash
-         (-let [(&plist :beg beg :end end) (elisp/thing-for-eval)]
-           (cons beg end)))))))
-
-(defun cb-elisp/init-dash ()
-  (use-package dash
-    :config (dash-enable-font-lock)))
+(defun cb-elisp/post-init-dash ()
+  (dash-enable-font-lock))
 
 (defun cb-elisp/init-flycheck-cask ()
   (use-package flycheck-cask
@@ -107,3 +63,7 @@ which require an initialization must be listed explicitly in the list.")
        `(highlight-defined-face-name-face
          ((((background dark))  (:foreground "#8D88AE"))
           (((background light)) (:foreground "#706D84"))))))))
+
+(defun cb-elisp/post-init-smart-ops ()
+  (define-smart-ops-for-mode 'emacs-lisp-mode
+    (smart-ops "." ",@" "," :pad-before t :pad-after nil)))
