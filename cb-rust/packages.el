27f6a0f9f13565442fd711f125bdce3898f74309
Chris Barrett <chris.d.barrett@me.com>
2015-10-02 09:15:02 +1300

    rust: delete extensions file

--- cb-rust/packages.el
+++ cb-rust/packages.el
@@ -2,13 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst cb-rust-packages
-  '(smart-ops))
-
 (eval-when-compile
   (require 'use-package nil t))
 
+(defconst cb-rust-packages
+  '(smart-ops))
+
 (defun cb-rust/post-init-smart-ops ()
   (define-smart-ops-for-mode 'rust-mode
-    (smart-ops ";" ":" "," :pad-before nil)
+    (smart-ops ";" "," "~" "&" ":" :pad-before nil)
+
+    (smart-ops "." "::"
+               :pad-before nil :pad-after nil
+               :action 'company-manual-begin)
+
+    ;; Position point inside template braces.
+    (smart-op "<>"
+              :pad-before nil :pad-after nil
+              :action (lambda (&rest _) (search-backward ">")))
+
     (smart-ops-default-ops)))
