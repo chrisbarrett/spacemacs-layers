40a8313b4a51ee72daeb36ea912a38117da2920d
Chris Barrett <chris.d.barrett@me.com>
2015-10-09 10:17:08 +1300

    project: tweak scala template

--- cb-project/project-skeletons/movio-scala-play-project/service/app/services/NewService.scala
+++ cb-project/project-skeletons/movio-scala-play-project/service/app/services/NewService.scala
@@ -3,5 +3,8 @@ package services
 import scala.concurrent.Future
 
 class NewService () {
-  def hello(): Future[String] = ???
+  def hello(): Future[String] = Future.successful {
+    print("hello from inside NewService!")
+    "hello"
+  }
 }
