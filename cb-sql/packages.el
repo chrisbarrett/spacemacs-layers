628b5428449f93dc014226eba5f57f2e295107a1
Chris Barrett <chris.d.barrett@me.com>
2015-10-18 17:06:12 +1300

    sql: use postgres by default

--- cb-sql/packages.el
+++ cb-sql/packages.el
@@ -15,5 +15,7 @@
     (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)))
 
 (defun cb-sql/init-sql ()
+  (setq sql-product 'postgres)
+  (add-hook 'sql-mode-hook 'sql-highlight-product)
   (with-eval-after-load 'sql
     (define-key sql-mode-map (kbd "C-c C-z") 'sql-product-interactive)))
