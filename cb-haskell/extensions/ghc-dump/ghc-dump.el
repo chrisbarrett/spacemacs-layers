244a1848adc459b0207c2c3491623ad6d873d024
Chris Barrett <chris.d.barrett@me.com>
2015-10-24 10:08:20 +1300

    haskell: improve C-- fontification

--- cb-haskell/extensions/ghc-dump/ghc-dump.el
+++ cb-haskell/extensions/ghc-dump/ghc-dump.el
@@ -69,7 +69,7 @@
 ;;;###autoload
 (defun ghc-dump-opt-cmm ()
   (interactive)
-  (ghc-dump--command-with-buffer-setup 'ignore "*ghc-opt-cmm*" "-ddump-opt-cmm"))
+  (ghc-dump--command-with-buffer-setup 'ghc-cmm-mode "*ghc-opt-cmm*" "-ddump-opt-cmm"))
 
 ;;;###autoload
 (defun ghc-dump-llvm ()
@@ -94,13 +94,38 @@
                                        "-ddump-splices" "-dsuppress-all"))
 
 ;;;###autoload
-(define-derived-mode ghc-stg-mode ghc-core-mode "GHC-STG")
-
-;;;###autoload
 (defun ghc-dump-stg ()
   (interactive)
   (ghc-dump--command-with-buffer-setup 'ghc-stg-mode "*ghc-stg*" "-ddump-stg"))
 
+;;;###autoload
+(define-derived-mode ghc-stg-mode ghc-core-mode "GHC-STG")
+
+;;;###autoload
+(define-derived-mode ghc-cmm-mode c-mode "C--")
+
+(defvar ghc-cmm-keywords
+  '("aborts" "align" "aligned" "also" "as" "big" "bits" "byteorder" "case"
+    "const," "continuation" "cut" "cuts" "else" "equal" "export" "foreign"
+    "goto" "if" "import" "in," "invariant" "invisible" "jump" "little" "memsize"
+    "pragma" "reads" "register," "return" "returns" "section" "semi" "span"
+    "stackdata" "switch" "target" "targets" "to," "typedef" "unicode" "unwinds"
+    "writes"))
+
+(defvar ghc-cmm-types
+  '("bits8" "bits16" "bits32" "bits64" "float32" "float64" "I8" "I16" "I32"
+    "CInt" "CLong" "I64" "CInt" "CLong" "L_" "F_" "D_"))
+
+(font-lock-add-keywords
+ 'ghc-cmm-mode
+ `((,(regexp-opt ghc-cmm-keywords) . font-lock-keyword-face)
+   (,(regexp-opt ghc-cmm-types) . font-lock-type-face)
+   (,(rx bol "=====" (* nonl)) . font-lock-comment-face)))
+
+(with-eval-after-load 'aggressive-indent
+  (add-to-list 'aggressive-indent-excluded-modes 'ghc-core-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'ghc-stg-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'ghc-cmm-mode))
 
 (provide 'ghc-dump)
 
