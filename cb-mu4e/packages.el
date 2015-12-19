221ce2ff27320cd53be0c55dd05604477dfec27b
Chris Barrett <chris.d.barrett@me.com>
2015-12-19 21:56:03 +1300

    mu4e: change attachment icon

--- cb-mu4e/packages.el
+++ cb-mu4e/packages.el
@@ -81,9 +81,8 @@
 
       ;; Set variables
 
-      (setq mu4e-confirm-quit nil)
       (setq mu4e-use-fancy-chars t)
-      (setq mu4e-headers-attach-mark (purecopy '("a" . "📎")))
+      (setq mu4e-headers-attach-mark (purecopy '("a" . "A")))
       (setq mu4e-headers-unread-mark (purecopy '("u" . "●")))
       (setq mu4e-hide-index-messages t)
 
