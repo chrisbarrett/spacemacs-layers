dded0a5485856b29f5d43911c75519f8f0846180
Chris Barrett <chris.d.barrett@me.com>
2015-12-29 09:15:04 +1300

    flycheck: enable flycheck pos-tip

--- cb-flycheck/packages.el
+++ cb-flycheck/packages.el
@@ -7,10 +7,13 @@
 
 (defconst cb-flycheck-packages
   '(flycheck
-    (flycheck-pos-tip :excluded t)))
+    flycheck-pos-tip))
 
 (defun cb-flycheck/post-init-flycheck ()
   (setq flycheck-display-errors-function 'flycheck-display-error-messages-unless-error-list)
   (setq flycheck-display-errors-delay 0.5)
   (setq flycheck-check-syntax-automatically '(mode-enabled idle-change save))
   (global-flycheck-mode +1))
+
+(defun cb-flycheck/post-init-flycheck-pos-tip ()
+  (setq flycheck-pos-tip-timeout 60))
