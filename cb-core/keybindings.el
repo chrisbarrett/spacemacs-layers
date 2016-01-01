adde11c7c4651e9a72ea5f667e2801f44ab30f4e
Chris Barrett <chris.d.barrett@me.com>
2016-01-02 10:57:58 +1300

    core: remove unused key binding

--- cb-core/keybindings.el
+++ cb-core/keybindings.el
@@ -12,8 +12,6 @@
 (global-set-key (kbd "C-c e e") 'toggle-debug-on-error)
 (global-set-key (kbd "C-x a a") 'align-regexp)
 
-(global-set-key (kbd "<f5>") 'compile)
-
 (bind-key* (kbd "C-<backspace>") 'core/kill-this-buffer)
 (bind-key (kbd "C-c k b") 'core/clean-buffers)
 
