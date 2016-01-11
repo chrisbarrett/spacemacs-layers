c9e623614f815b54eb9b7858c864dd491e669974
Chris Barrett <chris.d.barrett@me.com>
2016-01-11 20:03:53 +1300

    git: ensure M-x works in magit status buffers

--- cb-git/packages.el
+++ cb-git/packages.el
@@ -26,7 +26,8 @@
   (evil-set-initial-state 'git-commit-mode 'insert)
 
   (with-eval-after-load 'magit
-    (define-key magit-status-mode-map (kbd "&") 'git/browse-repo)))
+    (define-key magit-status-mode-map (kbd "S-SPC") #'helm-M-x)
+    (define-key magit-status-mode-map (kbd "&") #'git/browse-repo)))
 
 (defun cb-git/init-git-auto-commit-mode ()
   (use-package git-auto-commit-mode
