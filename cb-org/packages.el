b40ab02ced369f943068d6d88e6583dfe65f86c5
Chris Barrett <chris.d.barrett@me.com>
2016-01-24 15:00:29 +1300

    org: don't load any ob languages

--- cb-org/packages.el
+++ cb-org/packages.el
@@ -478,21 +478,6 @@ Do not scheduled items or repeating todos."
   :defer t
   :config
   (progn
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((python . t)
-       ;; (C . t)
-       ;; (ditaa . t)
-       (sh . t)
-       ;; (calc . t)
-       (scala . t)
-       ;; (sqlite . t)
-       (emacs-lisp . t)
-       (gnuplot . t)
-       ;; (ruby . t)
-       (clojure . t)
-       (haskell . t)))
-
     (setq org-src-fontify-natively t)
 
     (defvar org-edit-src-before-exit-hook nil
