d23c2cff515d2c4f896195977f9473d6e3ec2377
Chris Barrett <chris.d.barrett@me.com>
2016-01-26 20:15:12 +1300

    coq: restore some basic font lock prettifications.

--- cb-proof/packages.el
+++ cb-proof/packages.el
@@ -76,7 +76,18 @@
 
       (define-key coq-mode-map (kbd "M-RET")   'coq/meta-ret)
       (define-key coq-mode-map (kbd "C-c C-m") 'coq-insert-match)
-      (define-key coq-mode-map (kbd "RET")     'newline-and-indent))))
+      (define-key coq-mode-map (kbd "RET")     'newline-and-indent)
+
+      (dolist (mode '(coq-mode coq-response-mode coq-goals-mode))
+        (font-lock-add-keywords
+         mode
+         (list
+          (core/font-lock-replace-match (rx (and (or bol (any "," ":" "(" "[" ">")) (* space)) bow (group "forall") eow) 1 (string-to-char "∀"))
+          (core/font-lock-replace-match (rx (and (or bol (any "," ":" "(" "[" ">")) (* space)) bow (group "exists") eow) 1 (string-to-char "∃"))
+          (core/font-lock-replace-match (rx (or space eow) (group "->")  (or space eol bow)) 1 (string-to-char "→"))
+          (core/font-lock-replace-match (rx (or space eow) (group "=>")  (or space eol bow)) 1 (string-to-char "⇒"))
+          (core/font-lock-replace-match (rx (or space eow) (group ">=")  (or space eol bow)) 1 (string-to-char "≥"))
+          (core/font-lock-replace-match (rx (or space eow) (group "<=")  (or space eol bow)) 1 (string-to-char "≤"))))))))
 
 (defun cb-proof/init-proof-script ()
   (use-package proof-script
