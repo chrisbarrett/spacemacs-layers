59f9f49869b1607cb5eb19c9d82390022e0b139e
Chris Barrett <chris.d.barrett@me.com>
2016-03-07 19:05:44 +1300

    rust: remove unused funcs file

--- cb-rust/funcs.el
+++
@@ -1,7 +0,0 @@
-(defun rust/set-rust-library-path ()
-  "Set the search path for rust libraries."
-  (require 'flycheck)
-  (add-to-list 'flycheck-rust-library-path ".")
-  (when (projectile-project-p)
-    (add-to-list 'flycheck-rust-library-path (f-join (projectile-project-root) "src"))
-    (add-to-list 'flycheck-rust-library-path (f-join (projectile-project-root) "lib"))))
