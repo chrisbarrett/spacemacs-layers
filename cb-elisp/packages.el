359bfbc793979a8491621cbc5853f1749095d8f5
Chris Barrett <chris.d.barrett@me.com>
2016-03-09 19:56:45 +1300

    elisp: sharp quote fns

--- cb-elisp/packages.el
+++ cb-elisp/packages.el
@@ -28,18 +28,18 @@
     (define-key emacs-lisp-mode-map (kbd "C-c C-b") #'eval-buffer)
     (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'elisp/eval-dwim)
 
-    (define-key emacs-lisp-mode-map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
-    (evil-define-key 'normal emacs-lisp-mode-map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
-    (evil-define-key 'normal emacs-lisp-mode-map (kbd "K") 'elisp-slime-nav-describe-elisp-thing-at-point)
+    (define-key emacs-lisp-mode-map (kbd "M-.") #'elisp-slime-nav-find-elisp-thing-at-point)
+    (evil-define-key 'normal emacs-lisp-mode-map (kbd "M-.") #'elisp-slime-nav-find-elisp-thing-at-point)
+    (evil-define-key 'normal emacs-lisp-mode-map (kbd "K") #'elisp-slime-nav-describe-elisp-thing-at-point)
 
-    (spacemacs/set-leader-keys "ee" 'toggle-debug-on-error)
-    (spacemacs/set-leader-keys "hfl" 'find-library)
-    (spacemacs/set-leader-keys "hff" 'find-function)
-    (spacemacs/set-leader-keys "hfv" 'find-variable)
-    (spacemacs/set-leader-keys "hfF" 'find-face-definition)))
+    (spacemacs/set-leader-keys "ee" #'toggle-debug-on-error)
+    (spacemacs/set-leader-keys "hfl" #'find-library)
+    (spacemacs/set-leader-keys "hff" #'find-function)
+    (spacemacs/set-leader-keys "hfv" #'find-variable)
+    (spacemacs/set-leader-keys "hfF" #'find-face-definition)))
 
 (defun cb-elisp/post-init-eldoc ()
-  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
+  (add-hook 'emacs-lisp-mode-hook #'eldoc-mode))
 
 (defun cb-elisp/post-init-eval-sexp-fu ()
   (defun cb-elisp/configure-eval-sexp-fu ()
