962776ef774af2be27773d2c906441c79a9ddeab
Chris Barrett <chris.d.barrett@me.com>
2016-06-05 15:43:27 +1200

    Hide flycheck pos tips if error list buffer is showing

--- cb-flycheck/packages.el
+++ cb-flycheck/packages.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (eval-when-compile
+  (require 'dash nil t)
   (require 'use-package nil t))
 
 (defconst cb-flycheck-packages
@@ -10,12 +11,34 @@
     flycheck-pos-tip))
 
 (defun cb-flycheck/post-init-flycheck ()
-  (setq flycheck-display-errors-function 'flycheck-display-error-messages-unless-error-list)
-  (setq flycheck-display-errors-delay 0.5)
-  (setq flycheck-check-syntax-automatically '(mode-enabled idle-change save))
-  (global-flycheck-mode +1))
+  (use-package flycheck
+    :defer t
+    :config
+    (progn
+      (setq flycheck-display-errors-function 'flycheck-display-error-messages-unless-error-list)
+      (setq flycheck-display-errors-delay 0.5)
+      (setq flycheck-check-syntax-automatically '(mode-enabled idle-change save))
+      (global-flycheck-mode +1))))
 
 (defun cb-flycheck/post-init-flycheck-pos-tip ()
-  (setq flycheck-pos-tip-timeout 60))
+  (use-package flycheck-pos-tip
+    :after flycheck
+    :config
+    (progn
+      (setq flycheck-pos-tip-timeout 60)
+
+      ;; Hide error popups if the error list buffer is showing.
+
+      (defun cb-flycheck/error-list-showing? ()
+        (-some->> (get-buffer flycheck-error-list-buffer)
+                  (get-buffer-window-list)
+                  (-keep #'window-frame)))
+
+      (defun cb-flycheck/suppress-error-pos-tip (fn &rest args)
+        (if (cb-flycheck/error-list-showing?)
+            (apply flycheck-pos-tip-old-display-function args)
+          (apply fn args)))
+
+      (advice-add 'flycheck-pos-tip-error-messages :around #'cb-flycheck/suppress-error-pos-tip))))
 
 ;;; packages.el ends here
