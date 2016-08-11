f34d688dd0e3f4f47fe0cf60ed218f459fa32353
Chris Barrett <chris.d.barrett@me.com>
2016-08-11 21:21:33 +1200

    Remove docstring

--- cb-circe/packages.el
+++ cb-circe/packages.el
@@ -37,33 +37,7 @@
   '(circe
     persp-mode
     circe-notifications
-    (circe-show-channels :location local))
-  "The list of Lisp packages required by the cb-circe layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+    (circe-show-channels :location local)))
 
 (defun cb-circe/init-circe ()
   (use-package circe
